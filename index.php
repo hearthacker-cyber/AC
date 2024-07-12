416ebf02fd541f47bfc291fac9b3b1d8ebda0133
hearthacker-cyber <hearthacks2020@gmail.com>
2024-07-12 23:40:37 +0530

    help box removed

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-    <html lang="en">
+    <html lang="en" data-theme="dark">
 
     
 <!-- Mirrored from coderthemes.com/hyper/saas/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 29 Jul 2022 10:18:47 GMT -->
