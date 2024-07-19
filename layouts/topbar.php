4bc1c9470c8802b31dee68fc60b70fcb80d42191
hearthacker-cyber <hearthacks2020@gmail.com>
2024-07-19 23:52:18 +0530

    nothing

--- layouts/topbar.php
+++ layouts/topbar.php
@@ -288,7 +288,7 @@
                 </a>
 
                 <!-- item-->
-                <a href="javascript:void(0);" class="dropdown-item notify-item">
+                <a href="logout.php" class="dropdown-item notify-item">
                     <i class="mdi mdi-logout me-1"></i>
                     <span>Logout</span>
                 </a>
