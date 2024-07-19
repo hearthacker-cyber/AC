3cec5cdccf58c89081a024e40a32c04bcd4644a2
hearthacker-cyber <hearthacks2020@gmail.com>
2024-07-20 00:56:14 +0530

    all are finished

--- layouts/header.php
+++ layouts/header.php
@@ -7,8 +7,7 @@
 <head>
     <meta charset="utf-8" />
     <title>AC project demo</title>
-    <meta name="viewport" 
-      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
     <meta content="Coderthemes" name="author" />
@@ -26,28 +25,29 @@
     <link href="assets/css/vendor/buttons.bootstrap5.css" rel="stylesheet" type="text/css" />
     <link rel="manifest" href="../manifest.json">
 
-    <script>
-    navigator.serviceWorker.register("service-worker.js")
-    </script>
+    <style>
+    @media (max-width: 768px) {
+        .btn.btn-secondary.buttons-print {
+            display: none;
+        }
+    }
+    </style>
+    </s <?php session_start();
 
-<?php
-session_start();
+    // Check if the user is logged in
+    if (!isset($_SESSION['email'])) {
+        // Redirect to login page if not logged in
+        header('Location: login.html');
+        exit();
+    }
 
-// Check if the user is logged in
-if (!isset($_SESSION['email'])) {
-    // Redirect to login page if not logged in
-    header('Location: login.html');
-    exit();
-}
-?>
-
-    <body class="loading" data-layout-color="dark" data-leftbar-theme="dark" data-layout-mode="fluid" data-rightbar-onstart="true">
-        <!-- Begin page -->
-      
-
+    ?><body class="loading" data-layout-color="dark" data-leftbar-theme="dark" data-layout-mode="fluid"
+        data-rightbar-onstart="true">
+    < !-- Begin page -->
         <div class="wrapper"></div>
-    < <!-- <link href="assets/css/vendor/dataTables.bootstrap5.css" rel="s<link rel=" manifest" href="/manifest.json">
-        tylesheet" type="text/css" /> -->
+        < < !-- <link href="assets/css/vendor/dataTables.bootstrap5.css" rel="s<link rel=" manifest"
+            href="/manifest.json">
+            tylesheet" type="text/css" /> -->
 
 
 
@@ -56,5 +56,5 @@ if (!isset($_SESSION['email'])) {
 
 <body class="loading" data-layout-color="dark" data-leftbar-theme="dark" data-layout-mode="fluid"
     data-rightbar-onstart="true">
-    <!-- Begin page -->
-    <div class="wrapper"></div>
+    < !-- Begin page -->
+        <div class="wrapper"></div>
