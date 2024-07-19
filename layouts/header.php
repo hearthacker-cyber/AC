96cd5d435152f782a0b4d1c04fcb99e9f0a4773a
hearthacker-cyber <76662742+hearthacker-cyber@users.noreply.github.com>
2024-07-20 00:16:30 +0530

    Update header.php

--- layouts/header.php
+++ layouts/header.php
@@ -7,6 +7,8 @@
 <head>
     <meta charset="utf-8" />
     <title>AC project demo</title>
+    <meta name="viewport" 
+      content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"/>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description" />
     <meta content="Coderthemes" name="author" />
